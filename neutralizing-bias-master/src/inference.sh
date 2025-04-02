2bdeb5185c763478818b0b2882b012b5ab89a797
rkmaxhero <rounaqkhan12@gmail.com>
2025-04-01 22:27:16 -0400

    added scripts to perform prep and neutralization

---
+++ neutralizing-bias-master/src/inference.sh
@@ -0,0 +1,8 @@
+python joint/inference.py \
+       --extra_features_top --pre_enrich --activation_hidden \
+       --test_batch_size 1 --bert_full_embeddings --debias_weight 1.3 --token_softmax \
+       --pointer_generator --coverage \
+       --working_dir TEST \
+       --test bias_data/WNC/biased.word.test \
+       --checkpoint model.ckpt \
+       --inference_output TEST/output.txt
